6ac19c5f146830cae8d17fb168e64ce508f78df3
Harshit Vadodaria <harshitv95@gmail.com>
2020-11-08 18:25:01 -0500

    +BST and balanced (AVL)

---
+++ tree/AVL.hpp
@@ -0,0 +1,308 @@
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+#ifndef AVL_TREE
+#define AVL_TREE
+
+#define IS_BALANCED(node) ((node->balance < 2) && (node->balance > -2))
+#define BALANCE_TYPE(node) (node->balance > 0 ? LEFT_HEAVY : (node->balance < 0 ? RIGHT_HEAVY : BALANCED))
+
+#include "BST.hpp"
+
+template<typename Data_T>
+class AVL<Data_T> : public BST<Data_T> {
+public:
+    /***** Function Members *****/
+    AVL();
+
+    AVL(std::function<Data_T()> default_initializer);
+
+    /***** Others *****/
+    typedef enum {
+        ROOT_NODE, LEFT_NODE, RIGHT_NODE
+    } child_type;
+    typedef enum {
+        LEFT_ROTATE, RIGHT_ROTATE, LEFT_RIGHT_ROTATE, RIGHT_LEFT_ROTATE
+    } rotation_type;
+    typedef enum {
+        LEFT_HEAVY, RIGHT_HEAVY, BALANCED
+    } balance_type;
+
+protected:
+    class AVLNode : public BST<Data_T>::BinNode {
+    public:
+        int height, balance;
+        AVLNode *parent;
+        child_type childType;
+
+//        AVLNode() : BST::BinNode(), height(0), balance(0) {}
+
+        AVLNode(Data_T data) : BST::BinNode(data), height(0), balance(0) {}
+
+        void print();
+    };
+
+    int calcHeight(AVLNode *node);
+
+    int calcBalance(AVLNode *node);
+
+    AVLNode *findUnbalanced(AVLNode *rootNode);
+
+private:
+    /***** Private Function Members *****/
+    void postInsert(BinNode *node, BinNode *parentNode);
+
+    void postDelete(BinNode *parentNode);
+
+    void balance(AVLNode *node);
+
+    void rotate(AVLNode *rotateNode, rotation_type rotationType);
+
+}; // end of class declaration
+
+#endif
+
+//--- Definition of constructor
+template<typename Data_T>
+AVL<Data_T>::AVL() : BST<Data_T>() {}
+
+template<typename Data_T>
+AVL<Data_T>::AVL(std::function<Data_T()> default_initializer) : BST<Data_T>(default_initializer) {}
+
+// Private methods
+template<typename Data_T>
+void AVL<Data_T>::AVLNode::print() {
+    cout << this->data
+         << " (height: " << this->height
+         << ", balance: " << this->balance
+         << ", " << (
+                 this->childType == ROOT_NODE ? "ROOT" : (
+                         this->childType == LEFT_NODE ? "LEFT" : "RIGHT"
+                 ))
+         << " child of: " << (!this->parent ? -999 : this->parent->data)
+         << ")" << endl;
+}
+
+template<typename Data_T>
+void AVL<Data_T>::postInsert(BST<Data_T>::BinNode *node, BST<Data_T>::BinNode *parentNode) {
+    AVLNode *avlNode = ((AVLNode *) node);
+    avlNode->parent = ((AVLNode *) parentNode);
+
+    if (!parentNode)
+        avlNode->childType = ROOT_NODE;
+    else if (parentNode->left == node)
+        avlNode->childType = LEFT_NODE;
+    else if (parentNode->right == node)
+        avlNode->childType = RIGHT_NODE;
+
+    this->calcHeight((AVLNode *) this->myRoot);
+    if (avlNode->parent && avlNode->parent->parent)
+        this->balance(avlNode);
+}
+
+//void AVL::postDelete(BinNode *parentNode) {
+//    this->calcHeight((AVLNode*) this->myRoot);
+//    if (!parentNode) return;
+//
+//    AVLNode *parentAVLNode = (AVLNode*) parentNode;
+//    while (parentAVLNode && IS_BALANCED(parentAVLNode)) {
+//        switch (BALANCE_TYPE(parentAVLNode)) {
+//            case BALANCED:
+//                return;
+//            case RIGHT_HEAVY:
+//                parentAVLNode = (AVLNode*) parentAVLNode->right;
+//                break;
+//            case LEFT_HEAVY:
+//                parentAVLNode = (AVLNode*) parentAVLNode->left;
+//                break;
+//        }
+//    }
+//
+//    if (!parentAVLNode) return;
+//    switch (BALANCE_TYPE(parentAVLNode)) {
+//        case BALANCED:
+//        case LEFT_HEAVY:
+//            parentAVLNode = (AVLNode*) (parentAVLNode->left);
+//            break;
+//        case RIGHT_HEAVY:
+//            parentAVLNode = (AVLNode*) (parentAVLNode->right);
+//            break;
+//    }
+//    if (parentAVLNode->left)
+//        parentAVLNode = (AVLNode*) parentAVLNode->left;
+//    else if (parentAVLNode->right)
+//        parentAVLNode = (AVLNode*) parentAVLNode->right;
+//
+//    this->balance(parentAVLNode);
+//}
+
+template<typename Data_T>
+void AVL<Data_T>::postDelete(BST<Data_T>::BinNode *parentNode) {
+    this->calcHeight((AVLNode *) this->myRoot);
+    if (!parentNode)
+        return;
+    AVLNode *unbalanced;
+    while ((unbalanced = this->findUnbalanced((AVLNode *) this->myRoot))) {
+        switch (BALANCE_TYPE(unbalanced)) {
+            case BALANCED:
+            case LEFT_HEAVY:
+                unbalanced = (AVLNode *) unbalanced->left;
+                break;
+            case RIGHT_HEAVY:
+                unbalanced = (AVLNode *) unbalanced->right;
+                break;
+        }
+        if (unbalanced->left)
+            unbalanced = (AVLNode *) unbalanced->left;
+        else if (unbalanced->right)
+            unbalanced = (AVLNode *) unbalanced->right;
+
+        this->balance(unbalanced);
+    }
+}
+
+template<typename Data_T>
+AVL<Data_T>::AVLNode *AVL<Data_T>::findUnbalanced(AVL<Data_T>::AVLNode *rootNode) {
+    if (!rootNode || !IS_BALANCED(rootNode))
+        return rootNode;
+    AVLNode *unbalanced;
+    unbalanced = findUnbalanced((AVLNode *) rootNode->left);
+    if (unbalanced)
+        return unbalanced;
+    unbalanced = findUnbalanced((AVLNode *) rootNode->right);
+    return unbalanced;
+}
+
+template<typename Data_T>
+AVL<Data_T>::BinNode *AVL<Data_T>::initNode(Data_T data) {
+    AVLNode node = new AVLNode(data);
+    node.left = NULL;
+    node.right = NULL;
+    node.parent = NULL;
+    return node;
+}
+
+static int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+template<typename Data_T>
+int AVL<Data_T>::calcBalance(AVLNode *node) {
+    if (node->left && node->right)
+        node->balance =
+                ((AVLNode *) node->left)->height -
+                ((AVLNode *) node->right)->height;
+    else if (!node->left && !node->right)
+        node->balance = 0;
+    else if (!node->left)
+        node->balance = -1 - ((AVLNode *) node->right)->height;
+    else
+        node->balance = ((AVLNode *) node->left)->height + 1;
+}
+
+template<typename Data_T>
+int AVL<Data_T>::calcHeight(AVLNode *node) {
+    if (node == NULL) return -1;
+    ((AVLNode *) node)->parent = NULL;
+    ((AVLNode *) node)->childType = ROOT_NODE;
+
+    int leftHeight = calcHeight((AVLNode *) node->left);
+    int rightHeight = calcHeight((AVLNode *) node->right);
+
+    if (node->left) {
+        ((AVLNode *) node->left)->parent = node;
+        ((AVLNode *) node->left)->childType = LEFT_NODE;
+    }
+    if (node->right) {
+        ((AVLNode *) node->right)->parent = node;
+        ((AVLNode *) node->right)->childType = RIGHT_NODE;
+    }
+
+    node->height = 1 + max(leftHeight, rightHeight);
+    calcBalance(node);
+    return node->height;
+}
+
+template<typename Data_T>
+void AVL<Data_T>::balance(AVLNode *node) {
+    rotation_type rotationType;
+
+    AVLNode *balanceNode = node, *parent, *grandParent, *rotateNode;
+    while (
+            balanceNode &&
+            (parent = balanceNode->parent) &&
+            (grandParent = balanceNode->parent->parent)
+            ) {
+        if (!IS_BALANCED(grandParent)) {
+            if (parent->childType == LEFT_NODE) {
+                if (balanceNode->childType == LEFT_NODE) {
+                    rotationType = RIGHT_ROTATE;
+                    rotateNode = parent;
+                } else if (balanceNode->childType == RIGHT_NODE) {
+                    rotationType = LEFT_RIGHT_ROTATE;
+                    rotateNode = balanceNode;
+                }
+            } else if (parent->childType == RIGHT_NODE) {
+                if (balanceNode->childType == LEFT_NODE) {
+                    rotationType = RIGHT_LEFT_ROTATE;
+                    rotateNode = balanceNode;
+                } else if (balanceNode->childType == RIGHT_NODE) {
+                    rotationType = LEFT_ROTATE;
+                    rotateNode = parent;
+                }
+            }
+            cout << "Rotating [" << rotateNode->data << "]" << endl;
+            this->rotate(rotateNode, rotationType);
+            this->calcHeight((AVLNode *) this->myRoot);
+            return;
+        } else
+            balanceNode = balanceNode->parent;
+    }
+}
+
+template<typename Data_T>
+void AVL<Data_T>::rotate(AVLNode *rotateNode, rotation_type rotationType) {
+//    AVLNode *grandParent = rotateNode->parent->parent;
+    if (!rotateNode || !rotateNode->parent || rotateNode->childType == ROOT_NODE)
+        return;
+    bool isRoot;
+    AVLNode *parentOfRotated = NULL, *rotated;
+    rotated = rotateNode->parent;
+    isRoot = rotated->childType == ROOT_NODE;
+    parentOfRotated = rotated->parent;
+
+    switch (rotationType) {
+        case LEFT_ROTATE:
+            rotateNode->parent->right = rotateNode->left;
+            rotateNode->left = rotateNode->parent;
+            break;
+        case RIGHT_ROTATE:
+            rotateNode->parent->left = rotateNode->right;
+            rotateNode->right = rotateNode->parent;
+            break;
+        case LEFT_RIGHT_ROTATE: {
+            this->rotate(rotateNode, LEFT_ROTATE);
+            this->rotate(rotateNode, RIGHT_ROTATE);
+            return;
+        }
+        case RIGHT_LEFT_ROTATE: {
+            this->rotate(rotateNode, RIGHT_ROTATE);
+            this->rotate(rotateNode, LEFT_ROTATE);
+            return;
+        }
+    }
+
+    if (isRoot || parentOfRotated == NULL) // Is parent Root
+        this->myRoot = rotateNode;
+    else {
+        if (rotated->childType == LEFT_NODE) {
+            parentOfRotated->left = rotateNode;
+            rotateNode->parent = parentOfRotated;
+        } else if (rotated->childType == RIGHT_NODE) {
+            parentOfRotated->right = rotateNode;
+            rotateNode->parent = parentOfRotated;
+        }
+    }
+}
---
+++ tree/BST.hpp
@@ -0,0 +1,386 @@
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+#ifndef BINARY_SEARCH_TREE
+#define BINARY_SEARCH_TREE
+
+template<typename Data_T>
+class BST {
+public:
+    /***** Function Members *****/
+    BST();
+
+    BST(std::function<Data_T()> default_initializer);
+
+    bool empty() const;
+
+    Data_T *search(const Data_T &item) const;
+
+    void insert(const Data_T &item);
+
+    int nodeCount() const;
+
+    void preOrder() const;
+
+    void inOrder() const;
+
+    void postOrder() const;
+
+    void deleteNode(Data_T value);
+
+    /***** Others *****/
+    typedef enum {
+        PRE_ORDER, IN_ORDER, POST_ORDER
+    } traversal_order;
+    typedef enum {
+        LEAF_NODE, ONE_CHILD, TWO_CHILDREN
+    } delete_mode;
+
+protected:
+    std::function<Data_T()> default_init;
+
+    Data_T *search(const Data_T &item, Data_T *&parent) const;
+
+
+    /***** Node class *****/
+    class BinNode {
+    private:
+        Data_T data;
+    public:
+        BinNode *left;
+        BinNode *right;
+
+        // BinNode constructors
+        // Default -- data part is default int value; both links are null.
+//        BinNode()
+//                : left(0), right(0) {}
+
+        // Explicit Value -- data part contains item; both links are null.
+        BinNode(Data_T item)
+                : data(item), left(0), right(0) {}
+
+        virtual void print();
+
+        Data_T &getData() {
+            return this->data;
+        }
+
+    };// end of class BinNode declaration
+
+//    class DataBinNode : public BinNode {
+//    private:
+//        Data_T data;
+//    public:
+//        DataBinNode(Data_T data_in) : BinNode(true), data(data_in) {}
+//    };
+
+    /***** Data Members *****/
+    BinNode *myRoot;
+
+    /***** Protected Function Members *****/
+    virtual void postInsert(BinNode *node, BinNode *parentNode);
+
+    virtual void postDelete(Data_T deletedData, BinNode *parentNode);
+
+//    virtual BinNode *initNode();
+
+    virtual BinNode *initNode(const Data_T &data);
+
+    BinNode *smallest(BinNode *rootNode, BinNode *&parentNode, int &status);
+
+    BinNode *largest(BinNode *rootNode, BinNode *&parentNode, int &status);
+
+    BinNode *smallest(BinNode *rootNode);
+
+    BinNode *largest(BinNode *rootNode);
+
+private:
+    /***** Private Function Members *****/
+    int nodeCount(BinNode *node) const;
+
+    void traverse(BinNode *node, traversal_order order) const;
+
+    BinNode *searchNode(BinNode *startNode, const Data_T &data, BinNode *&parentNode);
+
+    void deleteNode(BinNode *node, BinNode *parentNode, delete_mode mode);
+
+}; // end of class declaration
+
+#endif
+
+
+//--- Definition of constructor
+template<typename Data_T>
+BST<Data_T>::BST()
+        : myRoot(0) {}
+
+template<typename Data_T>
+BST<Data_T>::BST(function<Data_T()> default_init)
+        : myRoot(0), default_init(default_init) {}
+
+template<typename Data_T>
+bool BST<Data_T>::empty() const { return myRoot == 0; }
+
+template<typename Data_T>
+Data_T *BST<Data_T>::search(const Data_T &item) const {
+    BinNode *locptr = myRoot;
+    while (locptr != 0) {
+        if (item < locptr->getData())       // descend left
+            locptr = locptr->left;
+        else if (locptr->getData() < item)  // descend right
+            locptr = locptr->right;
+        else if (locptr->getData() == item) // item found
+            return &(locptr->getData());
+    }
+    return nullptr;
+}
+
+template<typename Data_T>
+Data_T *BST<Data_T>::search(const Data_T &item, Data_T *&parent) const {
+    BinNode *locptr = myRoot;   // search pointer
+//    BinNode *parent = 0;        // pointer to parent of current node
+    while (locptr != 0) {
+        parent = locptr;
+        if (item < locptr->getData()) {      // descend left
+            parent = locptr;
+            locptr = locptr->left;
+        } else if (locptr->getData() < item) {  // descend right
+            parent = locptr;
+            locptr = locptr->right;
+        } else                           // item found
+            return &(locptr->getData());
+    }
+    return nullptr;
+}
+
+template<typename Data_T>
+void BST<Data_T>::insert(const Data_T &item) {
+    BinNode *parent;        // pointer to parent of current node
+    BinNode *locptr = this->searchNode(myRoot, item, parent);   // search pointer
+
+    if (!locptr) {                       // construct node containing item
+        locptr = this->initNode(item);
+        if (!parent) {             // empty tree
+            myRoot = locptr;
+            this->postInsert(myRoot, nullptr);
+        } else if (item < parent->getData()) { // insert to left of parent
+            parent->left = locptr;
+            this->postInsert(parent->left, parent);
+        } else {                          // insert to right of parent
+            parent->right = locptr;
+            this->postInsert(parent->right, parent);
+        }
+    } else
+        locptr->getData() = item;
+//    else
+//        cout << "Item already in the tree\n";
+}
+
+// Public methods to be called from user program
+template<typename Data_T>
+int BST<Data_T>::nodeCount() const {
+    return this->nodeCount(this->myRoot);
+}
+
+template<typename Data_T>
+void BST<Data_T>::preOrder() const {
+//    cout << "PreOrder Traversal:" << endl;
+    this->traverse(this->myRoot, PRE_ORDER);
+}
+
+template<typename Data_T>
+void BST<Data_T>::inOrder() const {
+//    cout << "InOrder Traversal:" << endl;
+    this->traverse(this->myRoot, IN_ORDER);
+}
+
+template<typename Data_T>
+void BST<Data_T>::postOrder() const {
+//    cout << "PostOrder Traversal:" << endl;
+    this->traverse(this->myRoot, POST_ORDER);
+}
+
+template<typename Data_T>
+void BST<Data_T>::deleteNode(Data_T value) {
+    BinNode *searchNode, *parentNode;
+    parentNode = NULL;
+    delete_mode mode = LEAF_NODE;
+    searchNode = this->searchNode(this->myRoot, value, parentNode);
+    if (searchNode->left && searchNode->right)
+        mode = TWO_CHILDREN;
+    else if (searchNode->left || searchNode->right)
+        mode = ONE_CHILD;
+    this->deleteNode(searchNode, parentNode, mode);
+//    cout << "Deleted " << value << " successfully, data in tree:" << endl;
+    this->traverse(this->myRoot, IN_ORDER);
+}
+
+
+// Private methods
+template<typename Data_T>
+int BST<Data_T>::nodeCount(BST<Data_T>::BinNode *node) const {
+    if (node == NULL || node == 0)
+        return 0;
+    return 1 + this->nodeCount(node->left) + this->nodeCount(node->right);
+}
+
+template<typename Data_T>
+typename BST<Data_T>::BinNode *
+BST<Data_T>::searchNode(BST<Data_T>::BinNode *startNode, const Data_T &data, BST<Data_T>::BinNode *&parentNode) {
+//    if (startNode == NULL || startNode == 0) return nullptr;
+    parentNode = nullptr;
+    while (startNode) {
+        if (startNode->getData() == data)
+            return startNode;
+        else if (data < startNode->getData()) {
+            parentNode = startNode;
+            startNode = startNode->left;
+        } else {
+            parentNode = startNode;
+            startNode = startNode->right;
+        }
+    }
+    return nullptr;
+}
+
+template<typename Data_T>
+void BST<Data_T>::BinNode::print() {
+    cout << this->getData() << endl;
+}
+
+template<typename Data_T>
+void BST<Data_T>::traverse(BST<Data_T>::BinNode *node, traversal_order order) const {
+    if (node == NULL || node == 0)
+        return;
+    BinNode *left = node->left;
+    BinNode *right = node->right;
+    switch (order) {
+        case PRE_ORDER:
+            node->print();
+            traverse(left, order);
+            traverse(right, order);
+            break;
+        case IN_ORDER:
+            traverse(left, order);
+            node->print();
+            traverse(right, order);
+            break;
+        case POST_ORDER:
+            traverse(left, order);
+            traverse(right, order);
+            node->print();
+            break;
+    }
+}
+
+template<typename Data_T>
+void
+BST<Data_T>::deleteNode(BST<Data_T>::BinNode *node, BST<Data_T>::BinNode *parentNode, BST<Data_T>::delete_mode mode) {
+    bool isRoot = (node == this->myRoot);
+    Data_T data = node->getData();
+//    if (isRoot)
+//        cout << "Deleting Root element : " << node->getData() << endl;
+//    else
+//        cout << "Deleting element [" << node->getData() << "] with parent [" << parentNode->getData() << "]" << endl;
+    switch (mode) {
+        case LEAF_NODE: {
+//            cout << "Deleting Leaf Node " << node->getData() << endl;
+            if (isRoot)
+                this->myRoot = NULL;
+            else {
+                if (parentNode->left == node) {
+//                    cout << "Deleting left child of parent: " << parentNode->getData() << endl;
+                    parentNode->left = NULL;
+                } else if (parentNode->right == node) {
+//                    cout << "Deleting right child of parent: " << parentNode->getData() << endl;
+                    parentNode->right = NULL;
+                }
+            }
+            break;
+        }
+        case ONE_CHILD: {
+            BinNode *child = node->left ? node->left : node->right;
+            if (isRoot)
+                this->myRoot = child;
+            else {
+                if (parentNode->left == node)
+                    parentNode->left = child;
+                else if (parentNode->right == node)
+                    parentNode->right = child;
+            }
+            break;
+        }
+        case TWO_CHILDREN: {
+            int status = 0;
+            BinNode *parentOfSmallestNode, *smallestNode;
+            smallestNode = smallest(node->right, parentOfSmallestNode, status);
+            if (parentOfSmallestNode != NULL && status == 1)
+                parentOfSmallestNode->left = smallestNode->right;
+            else
+                node->right = smallestNode->right;
+
+//            cout << "Found smallest : [" << smallestNode->getData() << "] with parent : [" << parentOfSmallestNode->getData() << "]";
+
+            smallestNode->left = node->left;
+            smallestNode->right = node->right;
+
+            if (isRoot) {
+                this->myRoot = smallestNode;
+            } else {
+                if (parentNode->left == node)
+                    parentNode->left = smallestNode;
+                else if (parentNode->right == node)
+                    parentNode->right = smallestNode;
+            }
+            break;
+        }
+    }
+    delete node;
+    this->postDelete(data, parentNode);
+}
+
+template<typename Data_T>
+typename BST<Data_T>::BinNode *BST<Data_T>::smallest(BinNode *rootNode, BinNode *&parentNode, int &status) {
+    if (rootNode->left == NULL) return rootNode;
+    parentNode = rootNode;
+    status = 1;
+    return smallest(rootNode->left, parentNode, status);
+}
+
+template<typename Data_T>
+typename BST<Data_T>::BinNode *BST<Data_T>::largest(BinNode *rootNode, BinNode *&parentNode, int &status) {
+    if (rootNode->right == NULL) return rootNode;
+    parentNode = rootNode;
+    status = 1;
+    return largest(rootNode->right, parentNode, status);
+}
+
+template<typename Data_T>
+typename BST<Data_T>::BinNode *BST<Data_T>::smallest(BinNode *rootNode) {
+    if (rootNode->left == NULL) return rootNode;
+    return smallest(rootNode->left);
+}
+
+template<typename Data_T>
+typename BST<Data_T>::BinNode *BST<Data_T>::largest(BinNode *rootNode) {
+    if (rootNode->right == NULL) return rootNode;
+    return largest(rootNode->right);
+}
+
+template<typename Data_T>
+void BST<Data_T>::postInsert(BST<Data_T>::BinNode *node, BST<Data_T>::BinNode *parentNode) {}
+
+template<typename Data_T>
+void BST<Data_T>::postDelete(Data_T data, BST<Data_T>::BinNode *parentNode) {}
+
+//template<typename Data_T>
+//BST<Data_T>::BinNode *BST<Data_T>::initNode() {
+//    return new BinNode();
+//}
+
+template<typename Data_T>
+typename BST<Data_T>::BinNode *BST<Data_T>::initNode(const Data_T &data) {
+    return new BinNode(data);
+}
